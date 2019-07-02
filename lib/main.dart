2aeeac2c3912725dd41fd865d47af77331cfee69
Samuel Mataraso <samuelmataraso@gmail.com>
2019-07-02 16:21:15 -0300

    feat/ starting implementing the home

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FlutterTube',
       home: Home(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,31 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 100.0,
+          child: Image.asset("images/youtube-logo.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        centerTitle: false,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
