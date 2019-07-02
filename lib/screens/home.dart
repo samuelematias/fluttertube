eb43247b6f57cc40e803be0a4a363ef7af31f2be
Samuel Mataraso <samuelmataraso@gmail.com>
2019-07-02 16:46:22 -0300

    feat/ starting implementing the search and adding some features on search like animated backbutton and cleanText on X.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../delegates/data_search.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,7 +25,9 @@ class Home extends StatelessWidget {
           ),
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {},
+            onPressed: () {
+              showSearch(context: context, delegate: DataSearch());
+            },
           )
         ],
       ),
